d3c8ebea4c459e449878555959b0fa9a8a2449ec
Athreya KS <65155021+athreya-ks@users.noreply.github.com>
2022-07-05 14:56:47 +0530

    Update main.tf

--- sleepy/main.tf
+++ sleepy/main.tf
@@ -14,7 +14,63 @@ resource "null_resource" "sleep12" {
 resource "null_resource" "sleep13" {
   depends_on = [null_resource.sleep12]
   provisioner "local-exec" {
-    command = "sleep 12m"
+    command = "sleep 29m"
+  }
+}
+
+resource "null_resource" "sleep14" {
+  depends_on = [null_resource.sleep]
+  provisioner "local-exec" {
+    command = "sleep 29m"
+  }
+}
+
+resource "null_resource" "sleep15" {
+  depends_on = [null_resource.sleep]
+  provisioner "local-exec" {
+    command = "sleep 29m"
+  }
+}
+
+resource "null_resource" "sleep16" {
+  depends_on = [null_resource.sleep]
+  provisioner "local-exec" {
+    command = "sleep 29m"
+  }
+}
+
+resource "null_resource" "sleep17" {
+  depends_on = [null_resource.sleep]
+  provisioner "local-exec" {
+    command = "sleep 29m"
+  }
+}
+
+resource "null_resource" "sleep18" {
+  depends_on = [null_resource.sleep]
+  provisioner "local-exec" {
+    command = "sleep 29m"
+  }
+}
+
+resource "null_resource" "sleep19" {
+  depends_on = [null_resource.sleep]
+  provisioner "local-exec" {
+    command = "sleep 29m"
+  }
+}
+
+resource "null_resource" "sleep20" {
+  depends_on = [null_resource.sleep]
+  provisioner "local-exec" {
+    command = "sleep 29m"
+  }
+}
+
+resource "null_resource" "sleep21" {
+  depends_on = [null_resource.sleep]
+  provisioner "local-exec" {
+    command = "sleep 15m"
   }
 }
 
