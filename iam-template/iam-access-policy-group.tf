1d854a5b70f2746af1c05c97a26412d33f4b580d
athreyaks <athreyaks@in.ibm.com>
2022-09-16 07:35:16 +0530

    added IAM template

--- iam-template/iam-access-policy-group.tf
+++ iam-template/iam-access-policy-group.tf
@@ -1,3 +1,21 @@
+provider "ibm" {
+    region             = "eu-de"
+	ibmcloud_api_key   = var.ibmcloud_api_key
+}
+terraform {
+  required_providers {
+    ibm = {
+      source = "IBM-Cloud/ibm"
+
+    }
+  }
+}
+
+variable ibmcloud_api_key {
+  description = "The IBM Cloud platform API key needed to deploy IAM enabled resources"
+  type        = string
+}
+
 resource "ibm_iam_access_group" "accgrp" {
   name        = "test"
   description = "New access group"
@@ -11,6 +29,7 @@ resource "ibm_iam_access_group_policy" "policy" {
     value = "dev"
   }
   transaction_id     = "terraformUserPolicy"
+  depends_on =[ibm_iam_access_group_policy.policy]
 }
 
 resource "ibm_iam_access_group_members" "accgroupmem" {
@@ -18,17 +37,6 @@ resource "ibm_iam_access_group_members" "accgroupmem" {
   ibm_ids         = ["test@in.ibm.com"]
   iam_service_ids = [ibm_iam_service_id.serviceID.id]
   iam_profile_ids = [ibm_iam_trusted_profile.profileID.id]
+  depends_on =[ibm_iam_access_group_policy.policy]
 }
 
-provider "ibm" {
-    region             = "eu-de"
-	ibmcloud_api_key   = var.ibmcloud_api_key
-}
-terraform {
-  required_providers {
-    ibm = {
-      source = "IBM-Cloud/ibm"
-
-    }
-  }
-}
